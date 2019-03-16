44d42fa4ccc24562ac6c7750c701938145210bfa
David Merrick <david@david-merrick.com>
2019-03-15 23:30:22 -0700

    Successfully fetched access token from Lyft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Typography, AppBar, Toolbar, Button } from "@material-ui/core";
+import { HashRouter, Route } from "react-router-dom";
+import Login from "./components/Login";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h5">Lyft Spend</Typography>
+            </Toolbar>
+          </AppBar>
+          <Route exact path="/" component={Login} />
+        </div>
+      </HashRouter>
     );
   }
 }
